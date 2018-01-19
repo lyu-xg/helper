dab6d9e24dcd04dc7d70172e94a242e14864641a
Xueguang (Luke) Lu <xueguang.lu@gmail.com>
2018-01-19 00:57:22 -0500

    added debt save support

--- mobile_fee.py
+++ mobile_fee.py
@@ -5,7 +5,7 @@ monthly_fee = 85
 def split(lst, mth_fee=monthly_fee, i=0):
     lst[i] -= mth_fee
     each = mth_fee/len(lst)
-    return [n+each for n in lst]
+    return [round(n+each, 2) for n in lst]
 
 def validate_mth(mth):
     return mth if mth <= 12 else mth%12
@@ -15,12 +15,17 @@ def print_fee(fees, mth_no):
     print('{}话费如下：'.format(m))
     for i, name in enumerate(names):
         if name not in no_show:
-            print('{}: {}'.format(name, round(fees[i], 2)))
+            print('{}: {}'.format(name, fees[i]))
     print('\n')
 
 def print_multi_mth(fees_matrix, starting_mth):
     for i, fees in enumerate(fees_matrix):
         print_fee(fees, starting_mth+i)
+
+def save_fee(fees):
+    with open('save.csv', 'w') as outfile:
+        outfile.write(','.join(names)+'\n')
+        outfile.write(','.join(map(str, fees))+'\n')
 
 def main(fees_matrix, starting_mth):
     if not fees_matrix:
@@ -30,7 +35,8 @@ def main(fees_matrix, starting_mth):
     print('亲爱的大家：')
     fees_matrix = [split(f) for f in fees_matrix]
     print_multi_mth(fees_matrix, starting_mth)
-    total_fees = [sum(fees_matrix[j][i] for j in range(len(fees_matrix))) for i in range(len(fees_matrix[0]))]
+    total_fees = [round(sum(fees_matrix[j][i] for j in range(len(fees_matrix))), 2) for i in range(len(fees_matrix[0]))]
+    save_fee(total_fees)
     print_fee(total_fees, '总共')
 
 def one_mth(fees, mth):
