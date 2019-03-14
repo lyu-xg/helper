d7781fe0a3412cee26aee419d0da55f9c6671120
XueGuang Lu <xueguang.lu@gmail.com>
2019-03-14 12:09:18 -0700

    updated fees

--- mobile_fee.py
+++ mobile_fee.py
@@ -2,9 +2,9 @@ no_show = ['XueGuang', 'Xu', 'QiuYuan']
 names = ['857-407-8735', 'XueGuang', 'QianHan', 'Xu', 'QiuYuan', 'MingNa', 'YuZhao', '949-402-9822', 'TianYi', 'TianXiao']
 monthly_fee = 85
 
-def split(lst, mth_fee=monthly_fee, i=0):
-    lst[i] -= mth_fee
-    each = mth_fee/len(lst)
+def split(lst):
+    # lst[i] -= mth_fee
+    each = monthly_fee/len(lst)
     return [round(n+each, 2) for n in lst]
 
 def validate_mth(mth):
@@ -33,7 +33,7 @@ def main(fees_matrix, starting_mth):
     if not all(len(l) == len(fees_matrix[0]) for l in fees_matrix):
         print('please pass in a well-shaped matrix - -'); return
     print('亲爱的大家：')
-    fees_matrix = [split(f, i=1) for f in fees_matrix]
+    fees_matrix = [split(f) for f in fees_matrix]
     print_multi_mth(fees_matrix, starting_mth)
     total_fees = [round(sum(fees_matrix[j][i] for j in range(len(fees_matrix))), 2) for i in range(len(fees_matrix[0]))]
     save_fee(total_fees)
@@ -45,8 +45,8 @@ def one_mth(fees, mth):
 if __name__ == '__main__':
 
     #   ['857-407-8735', 'XueGuang', 'Qianhan', 'Xu', 'QiuYuan', 'MingNa', 'YuZhao', '949-402-9822', 'TianYi', 'TianXiao']
-    main([[14.61, 110.95, 37.94, 66.61, 25.95, 25.95, 25.95, 32.75, 25.95, 46.93],
-          [14.61, 110.95, 37.94, 66.61, 25.95, 25.95, 25.95, 25.95, 25.95, 29.15],
-          [15.13, 111.43, 38.42, 67.09, 26.43, 26.43, 26.43, 26.43, 26.43, 26.43],
+    main([[15.93, 36.20, 27.21, 37.36, 27.21, 27.21, 27.21, 27.21, 30.19, 39.20],
+          [15.93, 36.20, 27.21, 27.21, 58.72, 39.73, 27.21, 27.21, 27.21, 39.20],
+          [29.46, 36.20, 27.21, 28.64, 89.86, 27.21, 27.21, 27.21, 26.43, 26.43],
           [15.13, 111.46, 38.42, 35.42, 26.43, 26.43, 26.43, 26.43, 26.43, 26.43],
-          [15.96, 112.26, 39.25, 36.25, 27.26, 27.26, 27.26, 27.26, 27.26, 27.26    ]],1)
+          [15.96, 112.26, 39.25, 36.25, 27.26, 27.26, 27.26, 27.26, 27.26, 27.26    ]],7)
